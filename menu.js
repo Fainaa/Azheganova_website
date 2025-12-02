da53c7bb21e917bcac90b93475f26192e975859a
Faina Azheganova <fainaazheganova@MacBook-Air-Faina.local>
2025-12-02 17:20:25 +0300

    some fixes

--- menu.js
+++ menu.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const mobileNav = document.querySelector('.mobile-nav');
-    const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
     
     hamburger.addEventListener('click', function() {
         this.classList.toggle('active');
